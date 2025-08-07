bc0598ce650825e603fefdf95bad84b820111215
IliaPopovsky <popovskyilia@gmail.com>
2025-08-07 16:39:13 +0300

    first version project

---
+++ thread_exit_handler/thread_exit_handler.c
@@ -0,0 +1,95 @@
+// thread_exit_handler   Обработчик выхода из потока
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <string.h>
+#include <errno.h> /* определение переменной errno */
+#include <stdarg.h> /* список аргументов переменной длины ISO C */
+
+#define MAXLINE 4096 /* максимальная длина строки */
+
+/*
+ * Выводит сообщение и возвращает управление в вызывающую функцию.
+ * Вызывающая функция определяет значение флага "errnoflag".
+ */
+static void err_doit(int errnoflag, int error, const char *fmt, va_list ap)
+{
+   char buf[MAXLINE];
+   vsnprintf(buf, MAXLINE-1, fmt, ap);
+   if (errnoflag)
+   snprintf(buf+strlen(buf), MAXLINE-strlen(buf)-1, ": %s",
+   strerror(error));
+   strcat(buf, "\n");
+   fflush(stdout); /* в случае, когда stdout и stderr - одно и то же устройство */
+   fputs(buf, stderr);
+   fflush(NULL); /* сбрасывает все выходные потоки */
+}
+
+/*
+ * Обрабатывает фатальные ошибки, не связанные с системными вызовами.
+ * Код ошибки передается в аргументе.
+ * Выводит сообщение и завершает работу процесса.
+ */
+void err_exit(int error, const char *fmt, ...)
+{
+   va_list ap;
+   va_start(ap, fmt);
+   err_doit(1, error, fmt, ap);
+   va_end(ap);
+   exit(1);
+}
+
+void cleanup(void *arg)
+{
+   printf("выход: %s\n", (char *)arg);
+}
+
+void *thr_fn1(void *arg)
+{
+   printf("запуск потока 1\n");
+   pthread_cleanup_push(cleanup, "поток 1, первый обработчик");
+   pthread_cleanup_push(cleanup, "поток 1, второй обработчик");
+   printf("поток 1, регистрация обработчиков закончена\n");
+   if(arg)
+      return((void *)1);
+   pthread_cleanup_pop(0);
+   pthread_cleanup_pop(0);
+   return((void *)1);
+}
+
+void *thr_fn2(void *arg)
+{
+   printf("запуск потока 2\n");
+   pthread_cleanup_push(cleanup, "поток 2, первый обработчик");
+   pthread_cleanup_push(cleanup, "поток 2, второй обработчик");
+   printf("поток 2, регистрация обработчиков закончена\n");
+   if(arg)
+      pthread_exit((void *)2);
+   pthread_cleanup_pop(0);
+   pthread_cleanup_pop(0);
+   pthread_exit((void *)2);
+}
+
+int main(void)
+{
+   int err;
+   pthread_t tid1, tid2;
+   void *tret;
+   
+   err = pthread_create(&tid1, NULL, thr_fn1, (void *)1);
+   if(err != 0)
+      err_exit(err, "невозможно создать поток 1");
+   err = pthread_create(&tid2, NULL, thr_fn2, (void *)1);
+   if(err != 0)
+      err_exit(err, "невозможно создать поток 2");
+   err = pthread_join(tid1, &tret);
+   if(err != 0)
+      err_exit(err, "невозможно присоединить поток 1");
+   printf("код выхода потока 1: %ld\n", (long)tret);
+   err = pthread_join(tid2, &tret);
+   if(err != 0)
+      err_exit(err, "невозможно присоединить поток 2");
+   printf("код выхода потока 2: %ld\n", (long)tret);
+   exit(0);
+}
