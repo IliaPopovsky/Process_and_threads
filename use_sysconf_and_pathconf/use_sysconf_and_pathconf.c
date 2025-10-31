38b8f7ef9992db970d4a2e3f09cad2dce37fa71b
IliaPopovsky <popovskyilia@gmail.com>
2025-10-31 19:15:23 +0300

    first version project

--- use_sysconf_and_pathconf/use_sysconf_and_pathconf.c
+++ use_sysconf_and_pathconf/use_sysconf_and_pathconf.c
@@ -98,45 +98,45 @@ int main(int argc, char *argv[])
 #endif
 
 #ifdef _SC_CLK_TCK
-   pr_sysconf("_SC_CLK_TCK =", _SC_CLK_TCK);
+   pr_sysconf("\n_SC_CLK_TCK =", _SC_CLK_TCK);
 #else
-   printf("идентификатор _SC_CLK_TCK не найден\n");
+   printf("\nидентификатор _SC_CLK_TCK не найден\n");
 #endif 
 
 #ifdef COLL_WEIGHTS_MAX
-   printf("COLL_WEIGHTS_MAX определен как %d\n", COLL_WEIGHTS_MAX+0);
+   printf("\nCOLL_WEIGHTS_MAX определен как %d\n", COLL_WEIGHTS_MAX+0);
 #else
-   printf("идентификатор COLL_WEIGHTS_MAX не найден\n");
+   printf("\nидентификатор COLL_WEIGHTS_MAX не найден\n");
 #endif  
 #ifdef _SC_COLL_WEIGHTS_MAX
    pr_sysconf("COLL_WEIGHTS_MAX =", _SC_COLL_WEIGHTS_MAX);
 #else
-   printf("идентификатор _SC_COLL_WEIGHTS_MAX не найден\n");
+   printf("\nидентификатор _SC_COLL_WEIGHTS_MAX не найден\n");
 #endif
 
 #ifdef DELAYTIMER_MAX
-   printf("DELAYTIMER_MAX определен как %d\n", DELAYTIMER_MAX+0);
+   printf("\nDELAYTIMER_MAX определен как %d\n", DELAYTIMER_MAX+0);
 #else
-   printf("идентификатор DELAYTIMER_MAX не найден\n");
+   printf("\nидентификатор DELAYTIMER_MAX не найден\n");
 #endif  
 #ifdef _SC_DELAYTIMER_MAX
    pr_sysconf("DELAYTIMER_MAX =", _SC_DELAYTIMER_MAX);
 #else
-   printf("идентификатор _SC_DELAYTIMER_MAX не найден\n");
+   printf("\nидентификатор _SC_DELAYTIMER_MAX не найден\n");
 #endif 
 
 #ifdef HOST_NAME_MAX
-   printf("HOST_NAME_MAX определен как %d\n", HOST_NAME_MAX+0);
+   printf("\nHOST_NAME_MAX определен как %d\n", HOST_NAME_MAX+0);
 #else
-   printf("идентификатор HOST_NAME_MAX не найден\n");
+   printf("\nидентификатор HOST_NAME_MAX не найден\n");
 #endif  
 #ifdef _SC_HOST_NAME_MAX
    pr_sysconf("HOST_NAME_MAX =", _SC_HOST_NAME_MAX);
 #else
-   printf("идентификатор _SC_HOST_NAME_MAX не найден\n");
+   printf("\nидентификатор _SC_HOST_NAME_MAX не найден\n");
 #endif 
 
-printf("Необязательные параметры :\n");
+printf("\nНеобязательные параметры :\n");
 #ifdef _POSIX_RAW_SOCKETS
    printf("_POSIX_RAW_SOCKETS определен как %ld\n", _POSIX_RAW_SOCKETS+0);
 #else
@@ -147,6 +147,17 @@ printf("Необязательные параметры :\n");
 #else
    printf("идентификатор _SC_RAW_SOCKETS не найден\n");
 #endif 
+
+#ifdef MAX_CANON
+   printf("\nMAX_CANON определен как %d\n", MAX_CANON+0);
+#else
+   printf("\nидентификатор MAX_CANON не найден\n");
+#endif  
+#ifdef _PC_MAX_CANON
+   pr_pathconf("MAX_CANON =", argv[1], _PC_MAX_CANON);
+#else
+   printf("\nидентификатор _PC_MAX_CANON не найден\n");
+#endif
    exit(0);
 
 }
@@ -170,6 +181,26 @@ static void pr_sysconf(char *mesg, int name)
        printf(" %ld\n", val);
    }
 } 
+
+static void pr_pathconf(char *mesg, char *path, int name)
+{
+   long val;
+   
+   fputs(mesg, stdout);
+   errno = 0;
+   if ((val = pathconf(path, name)) < 0) {
+       if (errno != 0) {
+           if (errno == EINVAL)
+               fputs(" (не поддерживается)\n", stdout);
+           else
+               err_sys("ошибка вызова pathconf, path = %s", path);
+       } else {
+           fputs(" (не поддерживается)\n", stdout);
+       }
+   } else {
+       printf(" %ld\n", val);
+   }
+}
    
    
                
